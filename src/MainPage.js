2cd90af781fc1a380b956c3cc20153a5ec34e42b
harshiththota <thota.harshit@gmail.com>
2020-06-21 23:01:33 +0530

    create MainPage

---
+++ src/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+import './MainPage.css';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className='head-container'>
+        <h1>Harshith Thota</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
